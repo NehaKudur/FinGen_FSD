ff890a2777ddb94f5bf861a9374ffc160de1227d
nishika sahu <sahunishika06@gmail.com>
2026-04-02 01:18:08 +0530

    remove firebase config, add example file

--- frontend/firebase.example.js
+++ frontend/firebase.example.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCV3z-v-blWjkpBKhOSkjnmtNGkYscIO_I",
-  authDomain: "fingen-78bc4.firebaseapp.com",
-  projectId: "fingen-78bc4",
-  storageBucket: "fingen-78bc4.firebasestorage.app",
-  messagingSenderId: "260373844448",
-  appId: "1:260373844448:web:2ba81813b7c5572b8adb85"
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
